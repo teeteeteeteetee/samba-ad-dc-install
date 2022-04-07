e63f3ea4ca499f2ee0a9c62076a6ce65fd657dc9
Tee <saltandtee@gmail.com>
2022-04-07 17:15:21 +0200

    yea

--- install.sh
+++ install.sh
@@ -7,35 +7,50 @@ fi
 
 repo=0
 
+normal=`echo "\033[m"`
+menu=`echo "\033[1;31m"` 
+number=`echo "\033[33m"` 
+bgred=`echo "\033[41m"`
+fgred=`echo "\033[31m"`
+green=`echo "\033[1;32m"`
+yellow=`echo "\033[1;33m"`
+
 update_package(){
     apt update
 }
 
 install_package(){
+    printf "${yellow}Removing krb5.conf (if exists)\n${normal}"
     rm -rf /etc/krb5.conf 2> /dev/null
+    printf "${yellow}Installing packages\n${normal}"
+    apt install acl attr samba samba-dsdb-modules samba-vfs-modules winbind libpam-winbind libnss-winbind libpam-krb5 krb5-config krb5-user dnsutils smbclient -y
+    printf "${yellow}Disabling services and enabling samba active directory\n${normal}"
+    systemctl disable --now smbd nmbd winbind
+    systemctl mask smbd nmbd winbind
+    systemctl unmask samba-ad-dc
+}
+
+remove_package(){
+    rm -rf /etc/krb5.conf 2> /dev/null
+    apt remove acl attr samba samba-dsdb-modules samba-vfs-modules winbind libpam-winbind libnss-winbind libpam-krb5 krb5-config krb5-user dnsutils smbclient -y
 }
 
 show_menu(){
 
     clear
 
-    normal=`echo "\033[m"`
-    menu=`echo "\033[1;31m"` #Blue
-    number=`echo "\033[33m"` #yellow
-    bgred=`echo "\033[41m"`
-    fgred=`echo "\033[31m"`
     printf "\n${menu}—————————————————————————————————————————————${normal}\n"
     printf "${menu}ᓚᘏᗢ${number} 1)${menu} Install Samba AD DC ${normal}\n"
     printf "${menu}ᓚᘏᗢ${number} 2)${menu} Install Samba AD DC (Change hostname) ${normal}\n"
     printf "${menu}ᓚᘏᗢ${number} 3)${menu} Remove Samba AD DC ${normal}\n"
-    printf "${menu}ᓚᘏᗢ${number} 4)${menu} Remove Samba AD DC (Full) ${normal}\n"
+    printf "${menu}ᓚᘏᗢ${number} 4)${menu} Remove Samba AD DC (With packages) ${normal}\n"
     printf "${menu}ᓚᘏᗢ${number} 5)${menu} Github Repository${normal}\n"
     printf "${menu}—————————————————————————————————————————————${normal}\n"
     printf "Please enter a menu option and enter or ${fgred}x to exit. ${normal}\n\n"
-    printf "\033[4;33mMade by Tee\033[0m —ฅ/ᐠ. ̫ .ᐟ\ฅ — \n"
+    printf "\033[4;33mMade by Tee${normal} —ฅ/ᐠ. ̫ .ᐟ\ฅ — \n"
 
     if [ "$repo" -eq "1" ]; then
-        printf "Repository:\033[1;32m https://github.com/lolitee/samba-ad-dc-install\n"
+        printf "Repository:${yellow} https://github.com/lolitee/samba-ad-dc-install\n"
         repo=0
     fi
     
@@ -53,6 +68,7 @@ show_menu(){
         3)
             ;;
         4)
+            remove_package
             ;;
         5)
             printf "\n"
